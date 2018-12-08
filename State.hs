55ed5d2315856b422d2b49d193e294af6d381020
jcelsocosta <jccl@cin.ufpe.br>
2018-12-08 18:17:44 -0200

    Add files via upload

---
+++ State.hs
@@ -0,0 +1,183 @@
+{-# LANGUAGE PackageImports #-}
+
+import "GLFW-b" Graphics.UI.GLFW as GLFW
+
+import Graphics.Gloss
+
+import Graphics.Gloss.Rendering as RS
+
+import System.Exit ( exitSuccess )
+
+import Control.Concurrent (threadDelay)
+
+import Control.Monad (when, unless)
+
+import Control.Monad.State.Strict
+
+
+
+width, height :: Int
+
+width  = 640
+
+height = 480
+
+
+
+type Pos = Point
+
+data Player = Player {position :: Pos}
+
+
+
+initialPlayer :: Player
+
+initialPlayer = Player (0,0)
+
+
+
+playerSize :: Float
+
+playerSize = 20
+
+
+
+main :: IO ()
+
+main = do
+
+    glossState <- initState
+
+    withWindow width height "Game-Demo" $ \win -> do
+
+          _ <- runStateT (loop win glossState) initialPlayer
+
+          exitSuccess
+
+
+
+loop :: Window -> RS.State -> StateT Player IO ()
+
+loop window glossState = do
+
+            lift $ threadDelay 20000
+
+            lift $ pollEvents
+
+            k <- lift $keyIsPressed window Key'Escape
+
+            l <- lift $keyIsPressed window Key'Left
+
+            r <- lift $keyIsPressed window Key'Right
+
+            u <- lift $keyIsPressed window Key'Up
+
+            d <- lift $ keyIsPressed window Key'Down
+
+            player <- get
+
+            let newState = movePlayer (l,r,u,d) player 10
+
+            put newState
+
+            lift $ renderFrame newState window glossState
+
+            unless k $ loop window glossState
+
+
+
+movePlayer :: (Bool, Bool, Bool, Bool) -> Player -> Float -> Player
+
+movePlayer direction player increment
+
+         | outsideOfLimits (position (move direction player increment)) playerSize = player
+
+         | otherwise = move direction player increment
+
+
+
+outsideOfLimits :: (Float, Float) -> Float -> Bool
+
+outsideOfLimits (xmon, ymon) size = xmon > fromIntegral width/2 - size/2 ||
+
+                                    xmon < (-(fromIntegral width)/2 + size/2) ||
+
+                                    ymon > fromIntegral height/2 - size/2 ||
+
+                                    ymon < (-(fromIntegral height)/2 + size/2)
+
+
+
+move :: (Bool, Bool, Bool, Bool) -> Player -> Float -> Player
+
+move (True, _, _, _) (Player (xpos, ypos)) increment = Player ((xpos - increment), ypos)
+
+move (_, True, _, _) (Player (xpos, ypos)) increment = Player ((xpos + increment), ypos)
+
+move (_, _, True, _) (Player (xpos, ypos)) increment = Player (xpos, (ypos + increment))
+
+move (_, _, _, True) (Player (xpos, ypos)) increment = Player (xpos, (ypos - increment))
+
+move (False, False, False, False) (Player (xpos, ypos)) _ = Player (xpos, ypos)
+
+
+
+renderFrame :: Player -> Window -> RS.State -> IO ()
+
+renderFrame (Player (xpos, ypos)) window glossState = do
+
+   displayPicture (width, height) white glossState 1.0 $ translate xpos ypos $ rectangleSolid playerSize playerSize
+
+   swapBuffers window
+
+
+
+withWindow :: Int -> Int -> String -> (GLFW.Window -> IO ()) -> IO ()
+
+withWindow windowWidth windowHeight title f = do
+
+    GLFW.setErrorCallback $ Just simpleErrorCallback
+
+    r <- GLFW.init
+
+    when r $ do
+
+        m <- GLFW.createWindow windowWidth windowHeight title Nothing Nothing
+
+        case m of
+
+          (Just win) -> do
+
+              GLFW.makeContextCurrent m
+
+              f win
+
+              GLFW.setErrorCallback $ Just simpleErrorCallback
+
+              GLFW.destroyWindow win
+
+          Nothing -> return ()
+
+        GLFW.terminate
+
+  where
+
+    simpleErrorCallback e s =
+
+        putStrLn $ unwords [show e, show s]
+
+
+
+keyIsPressed :: Window -> Key -> IO Bool
+
+keyIsPressed win key = isPress `fmap` GLFW.getKey win key
+
+
+
+isPress :: KeyState -> Bool
+
+isPress KeyState'Pressed   = True
+
+isPress KeyState'Repeating = True
+
+isPress _                  = False
---
+++ StateFRP.hs
@@ -0,0 +1,393 @@
+{-# LANGUAGE PackageImports, RecursiveDo #-}
+
+import "GLFW-b" Graphics.UI.GLFW as GLFW
+
+import Graphics.Gloss
+
+import Graphics.Gloss.Rendering 
+
+import System.Exit ( exitSuccess )
+
+import Control.Concurrent (threadDelay)
+
+import Control.Monad (when, unless, join)
+
+import Control.Monad.Fix (fix)
+
+import Control.Applicative ((<*>), (<$>))
+
+import FRP.Elerea.Simple
+
+import Foreign.C.Types (CDouble(..))
+
+import System.Random
+
+
+
+type Pos = Point
+
+data Player = Player { position :: Pos }
+
+type Hunting = Bool
+
+data Monster = Monster Pos MonsterStatus
+
+               deriving Show
+
+
+
+data MonsterStatus = Wander Direction Int
+
+                   | Hunting
+
+               deriving Show
+
+data Direction = WalkUp | WalkDown | WalkLeft | WalkRight
+
+                 deriving (Show, Enum, Bounded)
+
+
+
+instance Random Direction where
+
+  randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
+
+                       (x, g') -> (toEnum x, g')
+
+  random g = randomR (minBound, maxBound) g
+
+
+
+initialPlayer = Player (0, 0)
+
+
+
+initialMonster :: Monster
+
+initialMonster = Monster (200, 200) (Wander WalkUp wanderDist)
+
+width :: Int
+
+width = 640
+
+height :: Int
+
+height = 480
+
+playerSize :: Float
+
+playerSize = 20
+
+monsterSize :: Float
+
+monsterSize = 20
+
+monsterSpeed :: Float
+
+monsterSpeed = 5
+
+
+
+
+
+main :: IO ()
+
+main = do
+
+    (directionKeyGen, directionKeySink) <- external (False, False, False, False)
+
+    randomGenerator <- newStdGen
+
+    glossState <- initState
+
+    
+
+    withWindow width height "Game-Demo" $ \win -> do
+
+          network <- start $ do
+
+            directionKey <- directionKeyGen
+
+            hunted win directionKey randomGenerator glossState
+
+          fix $ \loop -> do
+
+               readInput win directionKeySink
+
+               join network
+
+               threadDelay 20000
+
+               esc <- keyIsPressed win Key'Escape
+
+               unless esc loop
+
+          exitSuccess
+
+
+
+hunted win directionKey randomGenerator glossState = mdo
+
+    player <- transfer2 initialPlayer (movePlayer 10) directionKey gameOver'
+
+    randomNumber <- stateful (undefined, randomGenerator) nextRandom
+
+    monster <- transfer3 initialMonster wanderOrHunt player randomNumber gameOver'
+
+    gameOver <- memo (playerEaten <$> player <*> monster)
+
+    gameOver' <- delay False gameOver
+
+    return $ renderFrame win glossState <$> player <*> monster <*> gameOver
+
+    where playerEaten player monster = distance player monster < (10^2  :: Float)
+
+          nextRandom (a, g) = random g
+
+
+
+readInput :: Window -> ((Bool, Bool, Bool, Bool) -> IO b) -> IO b
+
+readInput window directionKeySink = do
+
+    pollEvents
+
+    l <- keyIsPressed window Key'Left
+
+    r <- keyIsPressed window Key'Right
+
+    u <- keyIsPressed window Key'Up
+
+    d <- keyIsPressed window Key'Down
+
+    directionKeySink (l, r, u, d)
+
+
+
+movePlayer :: Float -> (Bool, Bool, Bool, Bool) -> Bool -> Player -> Player
+
+movePlayer _ _ True player = player
+
+movePlayer increment direction False player
+
+         | outsideOfLimits (position (move direction player increment)) playerSize = player
+
+         | otherwise = move direction player increment
+
+
+
+outsideOfLimits :: (Float, Float) -> Float -> Bool
+
+outsideOfLimits (xmon, ymon) size = xmon > fromIntegral width/2 - size/2 ||
+
+                                    xmon < (-(fromIntegral width)/2 + size/2) ||
+
+                                    ymon > fromIntegral height/2 - size/2 ||
+
+                                    ymon < (-(fromIntegral height)/2 + size/2)
+
+
+
+move :: (Bool, Bool, Bool, Bool) -> Player -> Float -> Player
+
+move (True, _, _, _) (Player (xpos, ypos)) increment = Player ((xpos - increment), ypos)
+
+move (_, True, _, _) (Player (xpos, ypos)) increment = Player ((xpos + increment), ypos)
+
+move (_, _, True, _) (Player (xpos, ypos)) increment = Player (xpos, (ypos + increment))
+
+move (_, _, _, True) (Player (xpos, ypos)) increment = Player (xpos, (ypos - increment))
+
+move (False, False, False, False) (Player (xpos, ypos)) _ = Player (xpos, ypos)
+
+
+
+wanderDist :: Int
+
+wanderDist = 40
+
+huntingDist :: Float
+
+huntingDist = 100
+
+
+
+wanderOrHunt :: Player
+
+                -> (Direction, b) -> Bool -> Monster -> Monster
+
+wanderOrHunt _ _ True monster = monster
+
+wanderOrHunt player (r, _) False monster = if close player monster
+
+                                                then hunt player monster
+
+                                                else wander r monster
+
+
+
+close :: Player -> Monster -> Bool
+
+close player monster = distance player monster < huntingDist^2
+
+
+
+distance :: Player -> Monster -> Float
+
+distance (Player (xpos, ypos)) (Monster (xmon, ymon) _) = (xpos - xmon)^2 + (ypos - ymon)^2
+
+
+
+-- if player is upper left quadrant, diagonal left
+
+-- means xpos > xmon and ypos > ymon
+
+hunt :: Player -> Monster -> Monster
+
+hunt (Player (xpos, ypos)) (Monster (xmon, ymon) _) = Monster ((xmon + (signum (xpos - xmon))*monsterSpeed), (ymon + (signum (ypos - ymon))*monsterSpeed)) Hunting
+
+
+
+-- turn in random direction
+
+wander :: Direction -> Monster -> Monster
+
+wander r (Monster (xmon, ymon) (Wander _ 0)) = Monster (xmon, ymon) (Wander r wanderDist)
+
+wander r (Monster (xmon, ymon) Hunting) = Monster (xmon, ymon) (Wander r wanderDist)
+
+-- go straight
+
+wander _ (Monster (xmon, ymon) (Wander direction n)) = do
+
+                   let currentDirection = continueDirection direction (outsideOfLimits (xmon, ymon) monsterSize)
+
+                   Monster
+
+                       (stepInCurrentDirection currentDirection (xmon, ymon) monsterSpeed)
+
+                       (Wander currentDirection (n-1))
+
+
+
+continueDirection :: Direction -> Bool -> Direction
+
+continueDirection WalkUp True = WalkDown
+
+continueDirection WalkDown True = WalkUp
+
+continueDirection WalkLeft True = WalkRight
+
+continueDirection WalkRight True = WalkLeft
+
+continueDirection direction False = direction
+
+
+
+stepInCurrentDirection :: Num a =>
+
+                          Direction -> (a, a) -> a -> (a, a)
+
+stepInCurrentDirection WalkUp (xpos, ypos)    speed = (xpos, ypos + speed)
+
+stepInCurrentDirection WalkDown (xpos, ypos)  speed = (xpos, ypos - speed)
+
+stepInCurrentDirection WalkLeft (xpos, ypos)  speed = (xpos - speed, ypos)
+
+stepInCurrentDirection WalkRight (xpos, ypos) speed = (xpos + speed, ypos)
+
+
+
+renderFrame :: Window
+
+               -> State -> Player -> Monster -> Bool -> IO ()
+
+renderFrame window glossState (Player (xpos, ypos)) (Monster (xmon, ymon) status) gameOver = do
+
+   displayPicture (width, height) white glossState 1.0 $ 
+
+     Pictures $ gameOngoing gameOver
+
+                             [renderPlayer xpos ypos,
+
+                              renderMonster status xmon ymon]
+
+   swapBuffers window
+
+
+
+renderPlayer :: Float -> Float -> Picture
+
+renderPlayer xpos ypos = Color black $ translate xpos ypos $ rectangleSolid playerSize playerSize
+
+
+
+renderMonster :: MonsterStatus -> Float -> Float -> Picture
+
+renderMonster status xpos ypos = Color (monsterColor status)  $ translate xpos ypos $ circleSolid playerSize
+
+                           where monsterColor Hunting = red
+
+                                 monsterColor (Wander _ _) = green
+
+
+
+-- adds gameover text if appropriate
+
+gameOngoing :: Bool -> [Picture] -> [Picture]
+
+gameOngoing gameOver pics = if gameOver then pics ++ [Color black $ translate (-100) 0 $ Scale 0.3 0.3 $ Text "Game Over"]
+
+                                        else pics
+
+
+
+withWindow :: Int -> Int -> String -> (GLFW.Window -> IO ()) -> IO ()
+
+withWindow width height title f = do
+
+    GLFW.setErrorCallback $ Just simpleErrorCallback
+
+    r <- GLFW.init
+
+    when r $ do
+
+        m <- GLFW.createWindow width height title Nothing Nothing
+
+        case m of
+
+          (Just win) -> do
+
+              GLFW.makeContextCurrent m
+
+              f win
+
+              GLFW.setErrorCallback $ Just simpleErrorCallback
+
+              GLFW.destroyWindow win
+
+          Nothing -> return ()
+
+        GLFW.terminate
+
+  where
+
+    simpleErrorCallback e s =
+
+        putStrLn $ unwords [show e, show s]
+
+
+
+keyIsPressed :: Window -> Key -> IO Bool
+
+keyIsPressed win key = isPress `fmap` GLFW.getKey win key
+
+
+
+isPress :: KeyState -> Bool
+
+isPress KeyState'Pressed   = True
+
+isPress KeyState'Repeating = True
+
+isPress _                  = False
